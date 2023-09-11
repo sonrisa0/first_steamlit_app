613d186105bd9e78ab1c4a9efb48e716cae43ad2
sonrisa0 <86060714+sonrisa0@users.noreply.github.com>
2023-09-12 00:39:33 +0100

    Update streamlit_app.py
    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,6 +28,9 @@ streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+streamlit.text(fruityvice_response.json()) #just writes the ata to the screen
+
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+
 
 
